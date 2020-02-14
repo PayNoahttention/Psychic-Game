ab1bbd37b0502d3c88f010103ae74110dcf5f092
PayNoahttention <NSolis1993@gmail.com>
2020-02-14 17:49:06 -0600

    Made a new game function. I think. Can't really test it. Should work in theory.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,24 @@
-Oh gawd I don't wanna do this.
+/* Let's start with some variables up here */
+
+let wins = 0, losses = 0;
+
+let answer;
+
+let triesLeft;
+
+/* function for new game. Google OP */
+
+function newGame() {
+    answer = String.fromCharCode(Math.floor(26 * Math.random()) + 97);
+
+    guesses = [];
+    triesLeft = 10;
+
+    $('#wins').text(wins);
+    $('#loses').text(loses);
+    $('#triesLeft').text(triesLeft);
+
+}
+
+$(document).ready(function() {
+    newGame();
