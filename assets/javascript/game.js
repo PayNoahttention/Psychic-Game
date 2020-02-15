613a62476f55ccb5630f88e4b37630068ad49f13
PayNoahttention <NSolis1993@gmail.com>
2020-02-14 18:48:44 -0600

    Okay. So I got into making the javascript work and stopped commiting. Note, what I sent before didn't work at all... This commit is all the JS workinggit add -A and adding instructions for the game. So neat. Enjoy.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
 /* Let's start with some variables up here */
 
-let wins = 0, losses = 0;
+let wins = 0 
+
+let losses = 0;
 
 let answer;
 
-let triesLeft;
+let triesLeft, guesses, guesses_space;
 
 /* function for new game. Google OP */
 
@@ -13,12 +15,49 @@ function newGame() {
 
     guesses = [];
     triesLeft = 10;
+    guesses_space = "";
 
     $('#wins').text(wins);
-    $('#loses').text(loses);
+    $('#losses').text(losses);
     $('#triesLeft').text(triesLeft);
+    $('#guesses').text(guesses_space);
 
 }
 
 $(document).ready(function() {
     newGame();
+/* Game tyme */
+    $(document).on("keypress", event => {
+        
+        const userInput = String.fromCharCode(event.which).toLowerCase();
+
+        if ("a" <= userInput && userInput <= "z") {
+            
+            if (!guesses.includes(userInput)) {
+                triesLeft--;
+
+                $("#triesLeft").text(triesLeft);
+                
+                
+                guesses.push(userInput);
+                guesses_space += userInput;
+
+                $("#guesses").text(guesses_space);
+
+              
+                if (userInput === answer) {
+                    wins++;
+
+                    newGame();
+
+             
+                } else if (triesLeft === 0) {
+                    losses++;
+
+                    newGame();
+
+                }
+            }
+        }
+    });
+});
